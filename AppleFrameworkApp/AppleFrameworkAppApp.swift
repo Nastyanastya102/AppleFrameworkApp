5f6efc82124754c1235d09c85044a66aeed6da1e
StacyKlyashtorna <Little.smart.bee@gmail.com>
2024-10-24 19:27:02 -0700

    Initial Commit

---
+++ AppleFrameworkApp/AppleFrameworkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkAppApp.swift
+//  AppleFrameworkApp
+//
+//  Created by Nastya Klyashtorna on 2024-10-24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
